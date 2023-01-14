f18ec36062a7414af8b4c82a25bb92b74584dd28
Muhammad Arif Billah <marifbillah524@gmail.com>
2023-01-15 05:22:02 +0700

    latest push

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '$#a12sMGvA1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/sipemas'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model) :
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.Text)
+    id_role = db.Column(db.Integer)
+
+    def __init__ (self,username,password,id_role):
+        self.username = username
+        if password != '':
+            self.password = bcrypt.generate_password_hash(password).decode('UTF-8')
+        self.id_role = id_role
+
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
+    nama_role = db.Column(db.String(100))
+
+    def __init__(self,nama_role):
+        self.nama_role = nama_role
 
 
 @app.route('/')
